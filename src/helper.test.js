df9a746d049b8c738fa1e8b7c541bee2e06b13d3
Coby Hong <cobyhong@gmail.com>
2021-01-20 10:45:24 -0800

    setup helper.test.js

---
+++ src/helper.test.js
@@ -0,0 +1,32 @@
+import { calculateWinner } from './helpers';
+
+test('2 + 2 should return 4', () => {
+    expect(2 + 2).toBe(4); // this test fails!
+  });
+
+test('should return X as the winner if it has the top row', () => {
+    // Arrange the test
+    const squares = ['X', 'X', 'X', 'O', null, 'O', null, null, null];
+    // Act
+    const winner = calculateWinner(squares);
+    // Assert
+    expect(winner).toBe('X');
+  });
+
+test('should return O as the winner if it has the descending diagnol', () => {
+    // Arrange the test
+    const squares = ['O', 'X', 'X', 'X', 'O', null, 'X', null, 'O'];
+    // Act
+    const winner = calculateWinner(squares);
+    // Assert
+    expect(winner).toBe('O');
+  });
+
+test('should return null as the winner if there is not yet a winner', () => {
+    // Arrange the test
+    const squares = ['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O'];
+    // Act
+    const winner = calculateWinner(squares);
+    // Assert
+    expect(winner).toBe(null);
+  });
